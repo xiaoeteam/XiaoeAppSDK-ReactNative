2dfe6adfd8b10b2904c8bdc306549ab110fe7602
EliPeng <jintaoliu@xiaoe-tech.com>
2020-06-28 14:21:04 +0800

    opt: 修改android工程

--- android/xeShopSDK/src/main/java/com/xiaoe/shop/sdk/rn/internal/XEShopDecoration.java
+++
@@ -1,7 +0,0 @@
-package com.xiaoe.shop.sdk.rn.internal;
-
-import com.xiaoe.shop.sdk.rn.XEShopActivity;
-
-public class XEShopDecoration {
-    public static XEShopActivity.NavStyle sNavStyle = new XEShopActivity.NavStyle(); // Use default nav style.
-}
